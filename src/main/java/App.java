faddc524125ec35978e3054e940547178d6a602d
alexa.rybin <Cfiekz17>
2020-12-07 14:38:40 +0300

    Задание выполнено

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHW1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHW1.getMessage());
+
+        HelloWorld beanHW2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = (Cat) applicationContext.getBean(Cat.class);
+        Cat beanCat2 = (Cat) applicationContext.getBean(Cat.class);
+
+        System.out.println(beanCat1.getMessage());
+        System.out.println(beanHW1.equals(beanHW2));
+        System.out.println(beanCat1.equals(beanCat2));
     }
 }
